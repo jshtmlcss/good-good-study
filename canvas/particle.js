db4c4181fe2719600bcf8fcacf64e6d323abbbe1
ffx0s <a13589949@gmail.com>
2017-09-14 17:07:01 +0800

    优化

--- canvas/particle.js
+++ canvas/particle.js
@@ -2,23 +2,38 @@
 import Observer from '../common/observer.js'
 
 /**
+ * element对象
+ */
+class Ele {
+  constructor (selector) {
+    this[0] = typeof selector === 'string' ? document.querySelector(selector) : selector
+    this.width = this[0].width
+    this.height = this[0].height
+    this.ctx = this[0].getContext('2d')
+  }
+}
+
+/**
  * cavnas对象，提供对画布的一些操作。
  */
 class Canvas {
   constructor (selector) {
-    this.el = typeof selector === 'string' ? document.querySelector(selector) : selector
-    this.width = this.el.width
-    this.height = this.el.height
-    this.ctx = this.el.getContext('2d')
+    // canvas 节点对象
+    this.el = new Ele(selector)
     // 粒子对象
     this.particle = new Particle()
+    // 动画对象
+    this.animation = new Animate(this.el, this.particle)
   }
   // 获取像素点
-  getImageData () {
-    let data = this.ctx.getImageData(0, 0, this.width, this.height).data
-    for (let x = 0; x < this.width; x += this.particle.grid) {
-      for (let y = 0; y < this.height; y += this.particle.grid) {
-        let pos = (y * this.width + x) * 4
+  getImageData (gridX = 6, gridY = 6) {
+    let data = this.el.ctx.getImageData(0, 0, this.el.width, this.el.height).data
+    // 清除上一次获取的粒子
+    this.particle.clear()
+    // 遍历像素点，找出有颜色的像素点
+    for (let x = 0; x < this.el.width; x += gridX) {
+      for (let y = 0; y < this.el.height; y += gridY) {
+        let pos = (y * this.el.width + x) * 4
         if (data[pos + 3] > 128) {
           this.particle.create({
             x,
@@ -33,88 +48,114 @@ class Canvas {
   }
   // 清除画布
   clearCanvas () {
-    this.ctx.clearRect(0, 0, this.width, this.height)
+    this.el.ctx.clearRect(0, 0, this.el.width, this.el.height)
   }
-  animate (options = {}) {
-    let that = this
-    let timer = null
-    let complete = { length: 0 }
-    let { type, speed, end, initDotEach, dotWidth, dotHeight, dotX, dotY } = Object.assign({
-      // 动画形式
-      type: 'easeOutQuad',
-      // 粒子移动速度
-      speed: 4,
-      // 动画结束回调
-      end: function () {},
+  animate (dotOptions = {}, animateOptions = {}) {
+    let { initDotEach, dotWidth, dotHeight, dotX, dotY, gridX, gridY } = Object.assign({
       // 粒子初始化遍历函数
       initDotEach: function () {},
       // 粒子大小
       dotWidth: 5,
       dotHeight: 5,
       // 粒子初始位置
-      dotX: that.width / 2,
-      dotY: that.height
+      dotX: this.el.width / 2,
+      dotY: this.el.height,
+      // 粒子的细粒度，越大粒子数量越少
+      gridX: 6,
+      gridY: 6
+    }, dotOptions)
+
+    // 获取像素数据
+    this.getImageData(gridX, gridY)
+
+    // 初始化粒子属性
+    this.particle.resetProps((dot, index) => {
+      dot.x = dotX
+      dot.y = dotY
+      dot.initX = dot.x
+      dot.initY = dot.y
+      dot.width = dotWidth
+      dot.height = dotHeight
+      initDotEach(dot, index)
+    })
+
+    this.animation.stop()
+    this.animation.init(animateOptions)
+    this.animation.execute()
+  }
+}
+
+// 动画对象
+class Animate {
+  constructor (el, particle) {
+    // canvas节点对象
+    this.el = el
+    // 粒子集合对象
+    this.particle = particle
+    // 用于停止动画
+    this.timer = null
+  }
+  init (options) {
+    // 动画选项
+    this.options = Object.assign({
+      // 动画形式
+      type: 'easeOutQuad',
+      // 粒子移动速度
+      speed: 4,
+      // 动画结束回调
+      end: function () {}
     }, options)
+    // 粒子完成数量
+    this.complete = { length: 0 }
+    // 动画是否进行中
+    this.isWorking = false
 
-    // 获取像素数据和初始化粒子属性
-    function init () {
-      that.getImageData()
-      that.particle.dots.forEach((dot, index) => {
-        dot.x = dotX
-        dot.y = dotY
-        dot.initX = dot.x
-        dot.initY = dot.y
-        dot.width = dotWidth
-        dot.height = dotHeight
-        // 增加dot薪属性
-        dot.id = index
-        dot.time = 150
-        dot.currTime = 0
-        dot.interval = parseInt(Math.random() * 500)
-        initDotEach(dot)
-        dot.draw(that.ctx)
-      })
+    return this
+  }
+  execute () {
+    let { type, speed, end } = this.options
+
+    // 数量一致则动画已结束
+    if (this.complete.length === this.particle.length) {
+      this.stop()
+      this.complete = { length: 0 }
+      this.isWorking = false
+      end()
+      return
     }
 
-    function loop () {
-      function getValue (dot, initValue, targetValue) {
-        return initValue === targetValue ? initValue : Tween[type](dot.currTime - dot.interval, initValue, targetValue - initValue, dot.time) || 0
-      }
+    // 清空画布
+    this.el.ctx.clearRect(0, 0, this.el.width, this.el.height)
 
-      that.clearCanvas()
-
-      that.particle.dots.forEach(dot => {
-        // 粒子当前时间小于总时间
-        if (dot.currTime < dot.time + dot.interval) {
-          // 错开粒子移动的时间，粒子当前时间大于间隔时间才移动
-          if (dot.currTime >= dot.interval) {
-            dot.x = getValue(dot, dot.initX, dot.dx)
-            dot.y = getValue(dot, dot.initY, dot.dy)
-          }
-          dot.currTime += Math.random() + speed
-        } else {
-          // 当前粒子动画已完成
-          dot.x = dot.dx
-          dot.y = dot.dy
-          if (!complete.hasOwnProperty(dot.id)) {
-            complete[dot.id] = 1
-            complete.length++
-          }
+    this.particle.dots.forEach(dot => {
+      // 粒子当前时间小于总时间
+      if (dot.currTime < dot.time + dot.interval) {
+        // 错开粒子移动的时间，粒子当前时间大于间隔时间才移动
+        if (dot.currTime >= dot.interval) {
+          dot.x = Animate.getValue(type, dot, dot.initX, dot.dx)
+          dot.y = Animate.getValue(type, dot, dot.initY, dot.dy)
         }
-        dot.draw(that.ctx)
-      })
-
-      if (complete.length === that.particle.length) {
-        end()
-        complete = null
-        window.cancelAnimationFrame(timer)
+        dot.currTime += Math.random() + speed
       } else {
-        timer = window.requestAnimationFrame(loop)
+        // 当前粒子动画已完成
+        dot.x = dot.dx
+        dot.y = dot.dy
+        if (!this.complete.hasOwnProperty(dot.id)) {
+          this.complete[dot.id] = 1
+          this.complete.length++
+        }
       }
-    }
+      dot.draw(this.el.ctx)
+    })
 
-    init()
-    loop()
+    this.timer = window.requestAnimationFrame(this.execute.bind(this))
+    this.isWorking = true
+  }
+  stop () {
+    window.cancelAnimationFrame(this.timer)
+  }
+  static getValue (type, dot, initValue, targetValue) {
+    return initValue === targetValue ? initValue : Tween[type](dot.currTime - dot.interval, initValue, targetValue - initValue, dot.time) || 0
   }
 }
 
@@ -122,18 +163,30 @@ class Canvas {
  * 粒子集合对象
  */
 class Particle {
-  constructor (options) {
+  constructor () {
     // 粒子集合
     this.dots = []
     // 粒子数量
     this.length = 0
-    // 用于控制粒子细粒度，值越小粒子越多
-    this.grid = 10
   }
   // 创建粒子
   create (options) {
     this.dots.push(new Dot(options))
     this.length++
+  }
+  clear () {
+    this.dots = []
+    this.length = 0
+  }
+  // 重置/新增粒子属性
+  resetProps (callback = function () {}) {
+    this.dots.forEach((dot, index) => {
+      dot.id = index
+      dot.time = 150
+      dot.currTime = 0
+      dot.interval = parseInt(Math.random() * 500)
+      callback(dot, index)
+    })
   }
 }
 
@@ -146,7 +199,7 @@ class Text extends Canvas {
     this.status = ''
   }
   draw (options) {
-    let ctx = this.ctx
+    let ctx = this.el.ctx
     let { text, x, y, font, fillStyle, textAlign } = Text.getOptions(options)
 
     this.clearCanvas()
@@ -155,8 +208,8 @@ class Text extends Canvas {
     ctx.fillStyle = fillStyle
     ctx.textAlign = textAlign
     if (x === 0 && y === 0) {
-      x = this.width / 2
-      y = this.height / 2
+      x = this.el.width / 2
+      y = this.el.height / 2
     }
     ctx.fillText(text, x, y)
 
@@ -191,7 +244,7 @@ class Picture extends Canvas {
   draw (options) {
     let { src, x, y } = Picture.getOptions(options)
     this.loadImage(src, () => {
-      this.ctx.drawImage(this.image, x, y, this.image.width, this.image.height)
+      this.el.ctx.drawImage(this.image, x, y, this.image.width, this.image.height)
       this.observer.emit('loaded')
     })
   }
